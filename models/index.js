7d709740389663b14d4fb43df1ddb8a3c76f1a42
James Maloney <malomedia7@gmail.com>
2019-04-29 12:22:00 -0700

    .

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require('../config.json')[env];
+var config    = require('../config/config.json')[env];
 var db        = {};
 
 console.log("CONFIG", config)
