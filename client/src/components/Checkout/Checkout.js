322791a47206bc6a3f5c7ba7e4aad70b159fb83f
David <dhahn123@gmail.com>
2019-05-24 14:08:37 -0700

    CustomerList.js: Column style list, Checkout.js: Stripe Payment Implemntation and Dynamic Rendering for Price amount

--- client/src/components/Checkout/Checkout.js
+++ client/src/components/Checkout/Checkout.js
@@ -1,59 +1,54 @@
-import React, { Component } from 'react';
-import axios from 'axios'
-import jwt_decode from 'jwt-decode'
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { Component, default as React } from "react";
+import { CardElement, injectStripe } from "react-stripe-elements";
 
-import Button from 'react-bootstrap/Button'
-
-import {
-  CardElement,
-  injectStripe,
-  StripeProvider,
-  Elements,
-} from 'react-stripe-elements';
-
-
-// You can customize your Elements to give it the look and feel of your site.
 const createOptions = () => {
   return {
     style: {
       base: {
-        fontSize: '16px',
-        color: '#424770',
-        fontFamily: 'Open Sans, sans-serif',
-        letterSpacing: '0.025em',
-        '::placeholder': {
-          color: '#aab7c4',
-        },
+        fontSize: "16px",
+        color: "#424770",
+        fontFamily: "Open Sans, sans-serif",
+        letterSpacing: "0.025em",
+        "::placeholder": {
+          color: "#aab7c4"
+        }
       },
       invalid: {
-        color: '#c23d4b',
-      },
+        color: "#c23d4b"
+      }
     }
-  }
+  };
 };
-
-class _CardForm extends Component {
+class Checkout extends Component {
   state = {
-    page: '',
-    errorMessage: '',
+    page: "",
+    errorMessage: ""
   };
 
   componentWillMount() {
     const loginToken = window.localStorage.getItem("token");
-    if (loginToken) this.setState({ decoded: jwt_decode(loginToken) })
-    if (window.localStorage.getItem('customer')) {
-      axios.get('/api/pages/search?pageId=' + window.localStorage.getItem('pageId'))
-        .then((resp) => {
-          console.log(resp)
+    if (loginToken) this.setState({ decoded: jwt_decode(loginToken) });
+    console.log(loginToken);
+    console.log(window.localStorage.getItem("customer"), "COMMENT!");
+    if (window.localStorage.getItem("customer")) {
+      axios
+        .get(
+          "/api/pages/search?pageId=" + window.localStorage.getItem("pageId")
+        )
+        .then(resp => {
+          console.log(resp, 'TESTER1');
           this.setState({
+            price: resp.data.response[0].price,
             page: resp.data.response[0],
             customer: true,
             s3Bucket: resp.data.bucket
-          })
+          });
         })
-        .catch((error) => {
-          console.error(error)
-        })
+        .catch(error => {
+          console.error(error);
+        });
     }
   }
 
@@ -63,74 +58,77 @@ class _CardForm extends Component {
     }
   };
 
-  handleSubmit = (evt) => {
-    evt.preventDefault();
-    console.log(this.props, this.state)
-    if (this.props.stripe) {
-      this.props.stripe.createToken().then(this.props.handleResult);
+  handleSubmit = ev => {
+    ev.preventDefault();
 
-      axios.post(`api/auth/page?customerId=${this.state.decoded.id}&pageId=${window.localStorage.getItem('pageId')}`)
-        .then((resp) => {
-          console.log(resp)
-          window.location = '/purchased'
-        }).catch((error) => {
-          console.error(error);
-        })
-    } else {
-      console.log("Stripe.js hasn't loaded yet.");
-    }
+    this.props.stripe
+      .createSource({
+        type: "card",
+        owner: {
+          name: "Jenny Rosen"
+        },
+        amount: Number(this.state.price) * 100
+      })
+      .then(({ source }) => {
+        console.log("Source:", source);
+        axios
+          .post(
+            `api/auth/page?customerId=${
+              this.state.decoded.id
+            }&pageId=${window.localStorage.getItem("pageId")}, data`
+          )
+          .then(resp => {
+            console.log(resp);
+            window.location = "/purchased";
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
-
   render() {
-
-    const { page } = this.state
+    const { page } = this.state;
 
     return (
-      <div className='center'>
-        <div className="CardDemo">
-          <form onSubmit={this.handleSubmit.bind(this)} style={{paddingBottom: 0}}>
-            <label style={{ width: 250 }}>
-              Card details
-            <CardElement
-                onChange={this.handleChange}
-                {...createOptions()}
+      <div className="center">
+        <form onSubmit={this.handleSubmit}>
+          <label style={{ width: 250 }}>
+            Card details
+            <CardElement onChange={this.handleChange} {...createOptions()} />
+          </label>
+          <div className="error" role="alert">
+            {this.state.errorMessage}
+          </div>
+          <button onClick={this.handleSubmit}>Confirm order</button>
+        </form>
+        {this.state.customer ? (
+          <div style={{ textAlign: "center" }}>
+            <div
+              className="page"
+              style={{ display: "flex", flex: "initial", margin: "1em auto" }}
+            >
+              <img
+                src={`https://s3-us-west-1.amazonaws.com/${
+                  this.state.s3Bucket
+                }/${page.imageLink}`}
+                style={{ width: 75, objectFit: "cover", marginRight: 20 }}
               />
-            </label>
-            <div className="error" role="alert">
-              {this.state.errorMessage}
-            </div>
-            <Button variant='success' onClick={this.handleSubmit}>Pay</Button>
-          </form>
-        </div>
-
-        {this.state.customer ?
-          <div style={{ textAlign: 'center' }}>
-            <div className='page' style={{ display: 'flex', flex: 'initial', margin: '1em auto' }}>
-              <img src={`https://s3-us-west-1.amazonaws.com/${this.state.s3Bucket}/${page.imageLink}`} style={{ width: 75, objectFit: 'cover', marginRight: 20 }} />
-              <div style={{ width: '100%', textAlign: 'left' }}>
+              <div style={{ width: "100%", textAlign: "left" }}>
                 <h4>{page.name}</h4>
                 <p>{page.description}</p>
               </div>
               <div>${page.price}</div>
             </div>
           </div>
-          :
-          <span></span>
-        }
+        ) : (
+          <span />
+        )}
       </div>
     );
   }
 }
 
-const CardForm = injectStripe(_CardForm);
-
-export default class CardDemo extends Component {
-  render() {
-    console.log(this.props)
-    return (
-      <div className='form'>
-        <CardForm handleResult={this.props.handleResult} />
-      </div>
-    );
-  }
-}
+export default injectStripe(Checkout);
---
+++ client/src/components/Content/CustomerList.js
@@ -0,0 +1,83 @@
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Moment from 'react-moment';
+import { NavLink } from 'react-router-dom';
+
+
+
+export default class CustomerList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            stats: [],
+            pages: []
+        }
+    }
+
+    componentWillMount() {
+        const loginToken = window.localStorage.getItem("token");
+        let decoded = ''
+        if (loginToken) decoded = jwt_decode(loginToken);
+
+        axios.get('api/customer/search?userId=' + decoded.id)
+            .then((resp) => {
+                console.log(resp)
+                this.setState({
+                    customer: resp.data.response,
+                    S3_BUCKET: resp.data.bucket
+                })
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }
+
+
+
+    render() {
+        return (
+            <ReactCSSTransitionGroup transitionName='fade' transitionAppear={true} transitionAppearTimeout={500} transitionEnter={false} transitionLeave={false}>
+                <div style={{backgroundColor: '#f9fafc', borderTop: '1px solid #ebecef'}}>
+                    <div className='main'>
+                        <div style={{ display: 'flex', flex: 1, justifyContent: 'center', margin: '20px auto' }}>
+                            <div>
+                                <DropdownButton title="Recent Customers">
+                                    <Dropdown.Item href="#/action-1">Recent Customers</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-2">Pages Purchased</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-3">Highest Value</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-4">Sort Criteria</Dropdown.Item>
+                                </DropdownButton>
+                            </div>
+                        </div>
+
+                        {/* List pages in table format */}
+                        <div style={{ display: 'flex', flex: 1, justifyContent: 'center'}}>
+                        <div style={{ display: 'flex', flexDirection: 'column' }}>
+                            {this.state.pages.map((page, i) =>
+                                <NavLink to={`/pages/${page.id}`} style={{ color: 'initial' }}>
+
+                                    <div key={i} className='page' style={{ display: 'flex' }}>
+                                        <img src={`https://s3-us-west-1.amazonaws.com/${this.state.S3_BUCKET}/${page.imageLink}`} style={{ width: 75, objectFit: 'cover', marginRight: 20 }} />
+                                        <div style={{width: '100%'}}>
+                                            <p>{page.name}</p>
+                                            <p style={{fontSize: 14, color: '#a4A5A8'}}>Item: <Moment format='M.DD.YYYY' date={page.createdAt} /></p>
+                                            <p style={{ alignItems: 'left'}}>
+                                            <span style={{fontSize: 14}}>Status: </span><span style={{fontSize: 14, color: 'green'}}>Paid</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </NavLink>
+                            )}
+                        </div>
+                        </div>
+
+                    </div >
+                </div>
+            </ReactCSSTransitionGroup >
+        )
+    }
+}
