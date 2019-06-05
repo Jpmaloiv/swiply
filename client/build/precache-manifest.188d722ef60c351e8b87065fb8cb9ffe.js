9a2b5f38083d558dff499e0cb82fe54e3c5e0883
James Maloney <malomedia7@gmail.com>
2019-06-05 11:00:18 -0700

    .

---
+++ client/build/precache-manifest.188d722ef60c351e8b87065fb8cb9ffe.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd17cd27778c0f2d1640f784b68347cb",
+    "url": "/static/media/sfnsdisplay-webfont.cd17cd27.ttf"
+  },
+  {
+    "revision": "ed5debe76eb21ed3d574",
+    "url": "/static/css/main.de77a7c4.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "408a8c1c959086231faee4e0985f0a44",
+    "url": "/static/media/welcome.408a8c1c.png"
+  },
+  {
+    "revision": "9e386eeb95875bc42f4f",
+    "url": "/static/js/2.c32c5cf9.chunk.js"
+  },
+  {
+    "revision": "1b6a7b50eb875b8391870202b3abc916",
+    "url": "/static/media/sfnsdisplay-webfont.1b6a7b50.eot"
+  },
+  {
+    "revision": "a78360b4a253d0084b43b15e4ec1d58b",
+    "url": "/static/media/sfnsdisplay-webfont.a78360b4.woff2"
+  },
+  {
+    "revision": "7ca6e99e708522087b00fff2d897fff7",
+    "url": "/static/media/sfnsdisplay-webfont.7ca6e99e.woff"
+  },
+  {
+    "revision": "ed5debe76eb21ed3d574",
+    "url": "/static/js/main.ac77a215.chunk.js"
+  },
+  {
+    "revision": "2c961d8a151233e3512e89081ce04d8a",
+    "url": "/static/media/sfnsdisplay-webfont.2c961d8a.svg"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "eb71f12f6de04a5d842eedf1941fbf04",
+    "url": "/static/media/congrats.eb71f12f.jpg"
+  },
+  {
+    "revision": "9e386eeb95875bc42f4f",
+    "url": "/static/css/2.89b97bc5.chunk.css"
+  },
+  {
+    "revision": "4e847cc34104419e60b2621ce36b6eaa",
+    "url": "/index.html"
+  }
+];
